92aed13dabfb6aa09fc1d040b653eef80378d9a5
jgbisuna <51989563+jose-bamboo@users.noreply.github.com>
2022-04-15 06:17:35 +0800

    Updated app name and added app icons

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CryptoProfitCalculator',
+      title: 'Hysto',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Inter',
